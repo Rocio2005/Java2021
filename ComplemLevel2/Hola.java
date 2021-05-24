5d27ade21d79f5113c9ac9e501d88a6fec0a69ed
Rocio2005 <70342681+Rocio2005@users.noreply.github.com>
2021-05-24 05:00:18 -0300

    Update Hola.java

--- ComplemLevel2/Hola.java
+++ ComplemLevel2/Hola.java
@@ -2,6 +2,6 @@ package Java2021.ComplemLevel2;
 public class Hola {
     
     public static void main (String [] args ) {
-        System.out.println("hola mundo e informatorio");
+        System.out.println("hola mundo");
     }
 }
