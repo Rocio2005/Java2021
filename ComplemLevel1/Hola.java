a38a98c2abe560c6b1cb862e2b2b27260bce2a8a
Rocio2005 <70342681+Rocio2005@users.noreply.github.com>
2021-05-24 04:59:29 -0300

    Update Hola.java

--- ComplemLevel1/Hola.java
+++ ComplemLevel1/Hola.java
@@ -3,6 +3,6 @@ package Java2021.ComplemLevel1;
 public class Hola {
     
     public static void main (String [] args ) {
-        System.out.println("hola mundo e informatorio");
+        System.out.println("hola mundo");
     }
 }
